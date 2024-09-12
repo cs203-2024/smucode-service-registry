bb6fedd455a8fa8cb342277ddd59b924a392a395
delectation1 <121929957+delectation1@users.noreply.github.com>
2024-09-12 21:29:33 +0800

    Enable eureka server

--- src/main/java/com/cs203/smucode/ServiceRegistryApplication.java
+++ src/main/java/com/cs203/smucode/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package com.cs203.smucode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 

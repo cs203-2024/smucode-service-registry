de98df0826e0a000e03d64e546080d8768bc5c7d
delectation1 <121929957+delectation1@users.noreply.github.com>
2024-09-12 21:23:58 +0800

    bootstrap spring initializr

---
+++ src/main/java/com/cs203/smucode/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.cs203.smucode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
